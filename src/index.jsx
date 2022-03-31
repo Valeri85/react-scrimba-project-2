1a6bd4561b52cc19a9ca4c476bb7a50d4281d3a7
Valerimakaridze <valeriousmonk85@gmail.com>
2022-03-31 17:54:20 +0400

    Remove HMR

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,3 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-if (import.meta.hot) {
-	import.meta.hot.accept();
-	import.meta.hot.dispose(() => {
-		App.$destroy();
-	});
-}
